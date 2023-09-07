fb89bad28c6eeea2600439c97de651ad304aad42
FaroukAmr <faroukamr508@gmail.com>
2023-09-07 10:56:03 +0300

    added title service to not found component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { TranslateService } from '@ngx-translate/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit, OnDestroy {
-  constructor(private translate: TranslateService) {}
+  constructor() {}
 
   ngOnInit() {}
 
--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,5 +1,7 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-not-found',
@@ -8,12 +10,21 @@ import { Component } from '@angular/core';
 })
 export class NotFoundComponent {
   displayedUrl: string = '';
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private translate: TranslateService,
+    private titleService: Title
+  ) {}
   ngOnInit() {
     this.route.url.subscribe((urlSegments) => {
       const requestedUrl = urlSegments.map((segment) => segment.path).join('/');
 
       this.displayedUrl = requestedUrl;
+      this.titleService.setTitle(this.translate.instant('notFound404'));
+
+      this.translate.onLangChange.subscribe(() => {
+        this.titleService.setTitle(this.translate.instant('notFound404'));
+      });
     });
   }
 }
